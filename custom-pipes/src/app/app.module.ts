8cbcd107d86aa41876d9204bc147bb55189da27c
paganini.marcell <paganini.marcell@gmail.com>
2022-10-12 21:33:32 -0300

    cadReal pipe created

--- custom-pipes/src/app/app.module.ts
+++ custom-pipes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CadRealPipe } from './pipes/cad-real.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadRealPipe
   ],
   imports: [
     BrowserModule
---
+++ custom-pipes/src/app/pipes/cad-real.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CadRealPipe } from './cad-real.pipe';
+
+describe('CadRealPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CadRealPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
